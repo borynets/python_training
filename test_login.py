88b89086207a242f233ecb162e96cfcac03f7c83
Nina <nina.borynets@gmail.com>
2019-01-03 21:56:49 +0800

    Выделены вспомогательные методы и классы

--- test_login.py
+++ test_login.py
@@ -1,40 +1,71 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from vacancy import Vacancy
 
 class LoginTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = WebDriver()
         self.driver.implicitly_wait(30)
         self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_login_test_case(self):
+
+    def test_login_test_case(self, username="romanovaua189@gmail.com", password ="123456"):
         driver = self.driver
-        driver.get("http://playtogether-001-site1.htempurl.com/login")
-        driver.find_element_by_xpath(u"(.//*[normalize-space(text()) and normalize-space(.)='Вход'])[1]/following::button[1]").click()
+        self.open_home_page(driver)
+        self.login(driver, username, password)
+        vacancy = Vacancy(title="Test tile", description="Test decsiprtion", minExperience=2);
+        self.create_new_vacancy(driver, vacancy) 
         driver.find_element_by_link_text(u"Выйти").click()
+
+    def open_home_page(self, driver):
+        driver.get("http://playtogether-001-site1.htempurl.com/login")
+
+    def login(self, driver, username, password):
+        # driver.find_element_by_link_text(u"Выйти").click()
         driver.find_element_by_name("userName").clear()
-        driver.find_element_by_name("userName").send_keys("romanovaua18@gmail.com")
+        driver.find_element_by_name("userName").send_keys(username)
         driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("199020")
-    
+        driver.find_element_by_name("password").send_keys(password)
+        driver.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='Вход'])[1]/following::button[1]").click()
+
+    def create_new_vacancy(self, driver, vacancy):
+        driver.find_element_by_link_text(u"Мои вакансии").click()
+        driver.find_element_by_link_text(u"Создать вакансию").click()
+        driver.find_element_by_name("title").click()
+        driver.find_element_by_name("title").clear()
+        driver.find_element_by_name("title").send_keys(vacancy.title)
+        driver.find_element_by_xpath(
+            u"(.//*[normalize-space(text()) and normalize-space(.)='Кого вы ищете *'])[1]/following::span[2]").click()
+        driver.find_element_by_xpath("//html").click()
+
+
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='x'])[1]/following::span[2]").click()
+
+        driver.find_element_by_name("description").clear()
+        driver.find_element_by_name("description").send_keys(vacancy.description)
+        driver.find_element_by_name("minExperience").clear()
+        driver.find_element_by_name("minExperience").send_keys(vacancy.minExperience)
+
+
+        driver.find_element_by_xpath(
+            "(.//*[normalize-space(text()) and normalize-space(.)='triphop'])[1]/following::button[1]").click()
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.driver.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -45,7 +76,7 @@ class LoginTestCase(unittest.TestCase):
                 alert.dismiss()
             return alert_text
         finally: self.accept_next_alert = True
-    
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
---
+++ vacancy.py
@@ -0,0 +1,7 @@
+
+
+class Vacancy:
+    def __init__(self, title, description, minExperience):
+        self.title = title
+        self.description = description
+        self.minExperience = minExperience
